cece374b03931431558be57281d3c72ba9054838
Nikhil <nikhilganeshpatil@gmail.com>
2023-06-26 10:13:13 +0530

    I have completed the creating a post functionality.

--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from .models import Profile
+from .models import Profile, Post
 
 # Create your views here.
 
@@ -120,4 +120,17 @@ def logout(request):
 
 @login_required(login_url='signin')
 def upload(request):
-    return HttpResponse('<h1>Upload View</h1>')
+    if request.method=='POST':
+        user=request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(
+            user=user,
+            image=image,
+            caption=caption
+        )
+        new_post.save();
+        return redirect('/');
+    else:
+        return redirect('/');
