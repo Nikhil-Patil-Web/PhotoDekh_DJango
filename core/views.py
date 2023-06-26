4b06c27aa59914298390001cfadb68953c38fc9d
Nikhil <nikhilganeshpatil@gmail.com>
2023-06-26 11:10:41 +0530

    I am done with the display of posts on the home screen.

--- core/views.py
+++ core/views.py
@@ -11,7 +11,8 @@ from .models import Profile, Post
 def index(request):
     user_object =User.objects.get(username= request.user.username)
     user_profile = Profile.objects.get(user=user_object)
-    return render(request, 'index.html', {'user_profile': user_profile})
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts': posts})
 
 
 ## now we will write a piece of code to register a user by the frontend. 
