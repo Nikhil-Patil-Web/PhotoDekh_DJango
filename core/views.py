021e94a07470d198802cf8def7e269c30b676dfd
Nikhil <nikhilganeshpatil@gmail.com>
2023-06-30 07:37:48 +0530

    I have updated the post feed such that only followers content is visible

--- core/views.py
+++ core/views.py
@@ -4,15 +4,33 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .models import Profile, Post, LikePost, FollowersCount
+from itertools import chain
 
 # Create your views here.
 
 @login_required(login_url='signin')
 def index(request):
+
     user_object =User.objects.get(username= request.user.username)
     user_profile = Profile.objects.get(user=user_object)
+
+    user_following_list=[];
+    feed =[];
+
+    user_following =FollowersCount.objects.filter(follower=request.user.username)
+
+    for users in user_following:
+        user_following_list.append(users.user)
+
+    for usernames in user_following_list:
+        feed_lists = Post.objects.filter(user=usernames)
+
+        feed.append(feed_lists);
+    
+    feed_list = list(chain(*feed))
+
     posts = Post.objects.all()
-    return render(request, 'index.html', {'user_profile': user_profile, 'posts': posts})
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts': feed_list})
 
 
 ## now we will write a piece of code to register a user by the frontend. 
